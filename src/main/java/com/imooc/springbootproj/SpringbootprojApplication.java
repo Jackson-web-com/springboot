ab89b884cd9aaccc94e2a250676479a052c0cf84
tom_glb <goodmorning_glb@atguigu.com>
2020-07-09 20:50:24 +0800

    first commit

---
+++ src/main/java/com/imooc/springbootproj/SpringbootprojApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.springbootproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//说明这是一个springboot应用的入口类
+@SpringBootApplication
+public class SpringbootprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootprojApplication.class, args);
+    }
+
+}
