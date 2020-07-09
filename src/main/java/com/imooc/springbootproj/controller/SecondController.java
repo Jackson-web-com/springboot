b3d1dde2fcbc3ffaf6d77b64492ae1dc322128ee
tom_glb <goodmorning_glb@atguigu.com>
2020-07-09 21:11:31 +0800

    third commit

---
+++ src/main/java/com/imooc/springbootproj/controller/SecondController.java
@@ -0,0 +1,7 @@
+package com.imooc.springbootproj.controller;
+
+public class SecondController {
+    public static void main(String[] args) {
+        System.out.println("this is two files");
+    }
+}
